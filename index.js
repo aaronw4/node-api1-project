5a6102827d86b0471c50cd8f00b9971c72b3b323
Aaron Williams <aaronrw4@yahoo.com>
2020-01-15 18:31:51 -0800

    Get request works

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// implement your API here
+const db = require('./data/db');
+const express = require('express');
+
+const server = express();
+
+server.listen(5000, () => {
+    console.log('Server listening on port 5000');
+});
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({sucess: false, err});
+        });
+});
